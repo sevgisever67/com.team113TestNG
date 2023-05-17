d984716e14e71daef545d598d994195af025c7c7
sevgi67 <elizafirs0@gmail.com>
2023-05-17 13:20:55 +0300

    17/05/23

---
+++ src/test/java/tests/day14_TestNGFramework/c01.java
@@ -0,0 +1,22 @@
+package tests.day14_TestNGFramework;
+
+import org.testng.annotations.Test;
+
+public class c01 {
+    @Test
+    public void youtubeTesti(){
+        System.out.println("Youtube");
+    }
+    @Test(priority = 3)
+
+    public void wiseTesti(){
+        System.out.println("Wise Quarter");
+    }
+
+    @Test
+    public void amazonTesti(){ // priority yazilmazsa 0 degeri alir
+        System.out.println("Amazon");
+    }
+
+}
+
